58e9dd3c9a1a87eaadfa7be2d44dd3ae7065b084
Radia Gulzan <radiagulzan@Radias-MacBook-Pro.local>
2022-07-14 15:58:34 +0100

    no secret key directly in the code because going on registration was an issue even though I can go to the database

--- application/__init__.py
+++ application/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY") 
+#It can't find os.getenv("SECRET_KEY") 
+app.config['SECRET_KEY'] = 'none'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://radiagulzan@db-relationship-practice-mysql:Password123!@db-relationship-practice-mysql.mysql.database.azure.com:3306/project_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
